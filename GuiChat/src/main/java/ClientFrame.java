da3e4a4f09b29eda2926af792a80d0053c60d828
oukongli <oukongli@gmail.com>
2015-01-14 23:32:00 +0800

    update GUI chat

--- GuiChat/src/main/java/ClientFrame.java
+++ GuiChat/src/main/java/ClientFrame.java
@@ -1,10 +1,13 @@
 import javax.swing.*;
 import javax.swing.text.Element;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.*;
 import java.util.List;
 
@@ -20,6 +23,10 @@ public class ClientFrame extends JFrame {
     private JList list;
     private String name;
     private DefaultListModel<String> model;
+    private Socket s;
+    private BufferedReader br;
+    private PrintWriter out;
+    private boolean stop = false;
 
     public ClientFrame(String name) {
         model = new DefaultListModel<String>();
@@ -28,8 +35,13 @@ public class ClientFrame extends JFrame {
         this.setSize(800, 600);
         this.name = name;
         this.setTitle("User Client" + name);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+//        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                out.println(Server.DISCONNECT_TOKEN);
+            }
+        });
 
         jta = new JTextArea();
         jsp1 = new JScrollPane(jta);
@@ -38,17 +50,32 @@ public class ClientFrame extends JFrame {
         jbtn = new JButton("send");
         jbtn.addActionListener(new BtnClick());
         list = new JList(model);
-        list.setFixedCellWidth(100);
+        list.setFixedCellWidth(150);
         jsp2 = new JScrollPane(list);
         jp = new JPanel();
-
-        this.setVisible(true);
 
         this.add(jsp1);
         jp.add(jtf);
         jp.add(jbtn);
         this.add(jp, BorderLayout.SOUTH);
         this.add(jsp2, BorderLayout.WEST);
+
+        connect();
+        this.setVisible(true);
+    }
+
+    private void connect() {
+        try {
+            s = new Socket(Server.HOST, Server.PORT);
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            out = new PrintWriter(s.getOutputStream(), true);
+            out.println(name);
+            new Thread(new ReceiveThread()).start();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private class KeyClick extends KeyAdapter {
@@ -64,20 +91,64 @@ public class ClientFrame extends JFrame {
         public void actionPerformed(ActionEvent e) {
             if (e.getSource() == jbtn) {
                 send();
-                List<String> l= list.getSelectedValuesList();
-                for (String str : l)
-                {
-                    System.out.println(str);
-                }
             }
         }
     }
 
-    private void send(){
+    private void send() {
         String word = jtf.getText();
         if (word == null || word.trim().equals(""))
             return;
-        jta.setText(jta.getText() + name + ":" + word + "\n");
         jtf.setText("");
+        out.println(word);
     }
+
+    private class ReceiveThread implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    if (stop)
+                        break;
+                    receive();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (s != null)
+                        s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.exit(0);
+            }
+        }
+    }
+
+    private void receive() throws IOException {
+        String str = br.readLine();
+        if (str.equals(Server.DISCONNECT_TOKEN))
+            close();
+        if (str.startsWith(Server.TRAN_USER_FLAG)) {
+            handleList(str);
+            return;
+        }
+        jta.setText(jta.getText() + str + "\n");
+    }
+
+    private void handleList(String str) {
+        String users = str.substring(Server.TRAN_USER_FLAG.length());
+        String[] us = users.split(",");
+        model.removeAllElements();
+        for (String u : us) {
+            model.addElement(u);
+        }
+    }
+
+    private void close() {
+        stop = true;
+    }
+
+
 }
--- GuiChat/src/main/java/Server.java
+++ GuiChat/src/main/java/Server.java
@@ -1,5 +1,133 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.*;
+
 /**
  * Created by oukongli on 2015/1/12.
  */
 public class Server {
+    public static final int PORT = 5858;
+    public static final String HOST = "127.0.0.1";
+    public static final String DISCONNECT_TOKEN = "disconnect";
+    public static final String TRAN_USER_FLAG = "connect:";
+
+    //private List<ServerThread> cs;
+    private Map<String,ServerThread> cs;
+
+
+    public static void main(String[] args) {
+        new Server().startup();
+    }
+
+    public void startup(){
+        ServerSocket ss = null;
+
+        try {
+            ss = new ServerSocket(PORT);
+//            cs = new ArrayList<ServerThread>();
+            cs = new HashMap<String, ServerThread>();
+            while (true){
+                try {
+                    Socket s = ss.accept();
+                    new Thread(new ServerThread(s)).start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (ss != null)
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+
+    }
+
+    private class ServerThread implements Runnable {
+        private Socket s;
+        private BufferedReader br;
+        private PrintWriter out;
+        private String name;
+        private boolean stop = false;
+
+        public ServerThread(Socket s) throws IOException {
+            this.s = s;
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            out = new PrintWriter(s.getOutputStream(), true);
+            name = br.readLine();
+            name += "[" + s.getInetAddress().getHostAddress() + "]";
+            send(name + "连接了");
+//            cs.add(this);
+            cs.put(name, this);
+            sendUser();
+        }
+
+        private void sendUser() {
+            String us = TRAN_USER_FLAG;
+            Set<String> keys = cs.keySet();
+            for (String u:keys){
+                us+=u+",";
+            }
+            send(us);
+        }
+
+        private void close(){
+            stop = true;
+            out.println(DISCONNECT_TOKEN);
+            cs.remove(name);
+
+            send(name+"断开连接了");
+            sendUser();
+        }
+
+        private void send(String msg){
+//            for (ServerThread st : cs){
+//                st.out.println(msg);
+//            }
+            Set<String> keys = cs.keySet();
+            for (String key : keys){
+                cs.get(key).out.println(msg);
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (true){
+                    if (stop)
+                        break;
+                    String str = br.readLine();
+                    if (str.equals(Server.DISCONNECT_TOKEN)){
+                        close();
+                        break;
+                    }
+                    send(name + str);
+                    System.out.println(name+":"+str);
+                }
+            } catch (SocketException e) {
+                System.out.println(name+ "非正常退出了");
+                close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }   finally {
+                if (s != null)
+                    try {
+                        s.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+            }
+        }
+    }
+
+
 }
