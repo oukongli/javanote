53c94386670798b85a392d533635dc3ee84d09da
oukongli <oukongli@gmail.com>
2015-01-21 22:58:17 +0800

    add dom4j

---
+++ dom4j/src/main/java/test1.java
@@ -0,0 +1,36 @@
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import java.io.File;
+import java.net.URL;
+import java.util.List;
+
+/**
+ * Created by oukongli on 2015/1/21.
+ */
+public class Test1 {
+    public static void main(String[] args) throws DocumentException {
+
+        /**
+         * 要获取类路径下的文件可以通过类.class.getClassLoader().getResource(filename)得到
+         */
+        /*URL url =  Test1.class.getClassLoader().getResource("test.xml");
+        File f = new File(url.getPath());
+        System.out.println(f.exists());*/
+
+        SAXReader reader = new SAXReader();
+        Document document = reader.read(Test1.class.getClassLoader().getResourceAsStream("test.xml"));
+
+        Element root = document.getRootElement();
+        System.out.println(root.getName());
+
+        List<Element> eles = root.elements();
+        for (Element e : eles){
+            System.out.println(e.getName());
+            System.out.println(e.attributeValue("id"));
+            System.out.println(e.elementText("title"));
+        }
+    }
+}
