a57b203d1643406362cd0fb9ac80ec186cace056
oukongli <oukongli@gmail.com>
2015-04-14 22:46:35 +0800

    servlet scope

---
+++ servletDemo/src/main/java/shdev/oukongli/java/HttpTest.java
@@ -0,0 +1,30 @@
+package shdev.oukongli.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Created by ou_kongli on 2015/4/14.
+ */
+public class HttpTest {
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket("www.baidu.com", 80);
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            out.println("GET / HTTP/1.1");
+            out.println("Host: www.baidu.com");
+            out.println("contentType: text/html");
+            out.println();
+            String str;
+            while((str=reader.readLine())!=null) {
+                System.out.println(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
