fcacb00ed8769ca1381723db6edf6114df35c157
oukongli <oukongli@gmail.com>
2015-05-06 21:35:09 +0800

    add junit test

---
+++ store/src/test/java/TestMyBatis.java
@@ -0,0 +1,14 @@
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Created by ou_kongli on 2015/5/6.
+ */
+
+
+public class TestMyBatis {
+    @Test
+    public void testDelete() {
+        Assert.assertEquals(3, 4);
+    }
+}
