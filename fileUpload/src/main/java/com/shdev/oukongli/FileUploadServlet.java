efb0ad4f34d0f0a5c67a7aaf09f6101308200036
oukongli <oukongli@gmail.com>
2015-05-23 22:24:49 +0800

    fileupload demo

---
+++ fileUpload/src/main/java/com/shdev/oukongli/FileUploadServlet.java
@@ -0,0 +1,64 @@
+package com.shdev.oukongli;
+
+import org.apache.commons.fileupload.FileItemIterator;
+import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.util.Streams;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by ou_kongli on 2015/5/23.
+ */
+public class FileUploadServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        boolean isMutipart = ServletFileUpload.isMultipartContent(request);
+        if (isMutipart) {
+            ServletFileUpload upload = new ServletFileUpload();
+            InputStream inputStream = null;
+            FileOutputStream fos = null;
+            try {
+                FileItemIterator iter = upload.getItemIterator(request);
+                while (iter.hasNext()) {
+                    FileItemStream fis = iter.next();
+                    inputStream = fis.openStream();
+                    if (fis.isFormField()) {
+                        System.out.println(fis.getFieldName() +":" + Streams.asString(inputStream));
+                    } else {
+                        System.out.println(fis.getName());
+                        String path = request.getSession().getServletContext().getRealPath("/upload");
+                        path += fis.getName();
+                        fos = new FileOutputStream(path);
+                        byte[] buf = new byte[1024];
+                        int len = 0;
+                        while((len = inputStream.read(buf)) > 0) {
+                            fos.write(buf,0, len);
+                        }
+                    }
+                }
+            } catch (FileUploadException e) {
+                e.printStackTrace();
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+                if (fos != null) {
+                    fos.close();
+                }
+            }
+        }
+    }
+}
---
+++ fileUpload/web/upload.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ou_kongli
+  Date: 2015/5/23
+  Time: 21:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<form method="post" action="fileupload" enctype="multipart/form-data">
+  Username:<input type="text" name="username" /><br/>
+  photo:<input type="file" name="photo" /><br/>
+  <input type="submit" value="submit">
+</form>
+</body>
+</html>
