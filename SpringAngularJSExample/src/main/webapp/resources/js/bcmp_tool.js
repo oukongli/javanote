3eba3311912a5e300302a649030665986b3c06c7
ouyangkongli <oukongli@gmail.com>
2016-11-07 01:17:58 +0800

    1. add button search 2. add search result table

--- SpringAngularJSExample/src/main/webapp/resources/js/bcmp_tool.js
+++ SpringAngularJSExample/src/main/webapp/resources/js/bcmp_tool.js
@@ -1,8 +1,41 @@
 'use strict';
 
-//var bcmp_tool = {};
-
 var app = angular.module('bcmp_tool',[]);
-app.controller('myCtrl', function ($scope) {
-    $scope.carname="BMW";
-});
+var bcmp_tool_controller = function ($scope, $http) {
+    $scope.testFunction = function () {
+        $http.get('users/userlist.json').success(function (userList) {
+            $scope.users = userList;
+        });
+    };
+};
+
+
+//var UserController = function($scope, $http) {
+//    $scope.fetchUsersList = function() {
+//        $http.get('users/userlist.json').success(function(userList){
+//            $scope.users = userList;
+//        });
+//    };
+//
+//    $scope.addNewUser = function(newUser) {
+//        $http.post('users/addUser/' + newUser).success(function() {
+//            $scope.fetchUsersList();
+//        });
+//        $scope.userName = '';
+//    };
+//
+//    $scope.removeUser = function(user) {
+//        $http.delete('users/removeUser/' + user).success(function() {
+//            $scope.fetchUsersList();
+//        });
+//    };
+//
+//    $scope.removeAllUsers = function() {
+//        $http.delete('users/removeAllUsers').success(function() {
+//            $scope.fetchUsersList();
+//        });
+//
+//    };
+//
+//    $scope.fetchUsersList();
+//};
