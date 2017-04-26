20fabeef07a92ff41b6b71f97c2d114191918089
ouyangkongli <oukongli@gmail.com>
2017-04-27 01:11:06 +0800

    terminate thread -1

---
+++ thinkingInJava/src/com/shdev/note/thread/concurrency/AtomicIntegerTest.java
@@ -0,0 +1,51 @@
+package com.shdev.note.thread.concurrency;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by ouyangkongli on 2017/4/26.
+ */
+public class AtomicIntegerTest implements Runnable {
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    public int getValue() {
+        return atomicInteger.get();
+    }
+
+    private void evenIncrement() {
+        atomicInteger.addAndGet(2);
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            evenIncrement();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.err.println("aborting");
+                System.exit(0);
+            }
+        }, 5000);
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        AtomicIntegerTest atomicIntegerTest = new AtomicIntegerTest();
+        executorService.execute(atomicIntegerTest);
+        while (true) {
+            int val = atomicIntegerTest.getValue();
+            if (val % 2 != 0) {
+                System.out.println(val);
+                System.exit(0);
+            }
+        }
+    }
+
+}
