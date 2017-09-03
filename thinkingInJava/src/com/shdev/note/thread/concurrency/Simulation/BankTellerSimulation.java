e5af4b1bd28eb7a0d45d8ab5af96cc14f22a2517
ouyangkongli <oukongli@gmail.com>
2017-09-04 00:22:12 +0800

    BankTellerSimulation

---
+++ thinkingInJava/src/com/shdev/note/thread/concurrency/Simulation/BankTellerSimulation.java
@@ -0,0 +1,209 @@
+package com.shdev.note.thread.concurrency.Simulation;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ouyangkongli on 2017/9/3.
+ */
+class Customer {
+    private final int serviceTime;
+
+    public Customer(int serviceTime) {
+        this.serviceTime = serviceTime;
+    }
+
+    public int getServiceTime() {
+        return serviceTime;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + serviceTime + "]";
+    }
+}
+
+class CustomerLine extends ArrayBlockingQueue<Customer> {
+
+    public CustomerLine(int capacity) {
+        super(capacity);
+    }
+
+    @Override
+    public String toString() {
+        if (this.size() == 0) {
+            return "[Empty]";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Customer customer : this) {
+            stringBuilder.append(customer);
+        }
+        return stringBuilder.toString();
+    }
+}
+
+class CustomerGenerator implements Runnable {
+    private CustomerLine customers;
+    private static Random random = new Random(47);
+
+    public CustomerGenerator(CustomerLine customers) {
+        this.customers = customers;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                TimeUnit.MILLISECONDS.sleep(random.nextInt(300));
+                customers.add(new Customer(random.nextInt(1000)));
+            }
+        } catch (InterruptedException e) {
+            System.out.println("CustomerGenerator interrupted");
+        }
+        System.out.println("CustomerGenerator terminating");
+    }
+}
+
+class Teller implements Runnable, Comparable<Teller> {
+    private static int counter = 0;
+    private final int id = counter++;
+    private int customersServed = 0;
+    private CustomerLine customers;
+    private boolean servingCustomerLine = true;
+
+    public Teller(CustomerLine customers) {
+        this.customers = customers;
+    }
+
+    @Override
+    public synchronized int compareTo(Teller other) {
+        return customersServed < other.customersServed ? -1 : (customersServed == other.customersServed ? 0 : 1);
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                Customer customer = customers.take();
+                TimeUnit.MILLISECONDS.sleep(customer.getServiceTime());
+                synchronized (this) {
+                    customersServed++;
+                    while (!servingCustomerLine) {
+                        wait();
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            System.out.println(this + " interrupted");
+        }
+        System.out.println(this + " terminating");
+    }
+
+    public synchronized void doSomethingElse() {
+        customersServed = 0;
+        servingCustomerLine = false;
+    }
+
+    public synchronized void serveCustomerLine() {
+        assert !servingCustomerLine : "already serving: " + this;
+        servingCustomerLine = true;
+        notifyAll();
+    }
+
+    @Override
+    public String toString() {
+        return "Teller " + id + " ";
+    }
+
+    public String shortString() {
+        return "T" + id;
+    }
+}
+
+class TellerManager implements Runnable {
+    private ExecutorService executorService;
+    private CustomerLine customers;
+    private PriorityQueue<Teller> workingTellers = new PriorityQueue<>();
+    private Queue<Teller> tellersDoingOtherThings = new LinkedList<>();
+    private int adjustmentPeriod;
+    private static Random random = new Random(47);
+
+    public TellerManager(ExecutorService executorService, CustomerLine customers, int adjustmentPeriod) {
+        this.executorService = executorService;
+        this.customers = customers;
+        this.adjustmentPeriod = adjustmentPeriod;
+        Teller teller = new Teller(customers);
+        executorService.execute(teller);
+        workingTellers.add(teller);
+    }
+
+    public void adjustTellerNumber() {
+        if (customers.size() / workingTellers.size() > 2) {
+            if (tellersDoingOtherThings.size() > 0) {
+                Teller teller = tellersDoingOtherThings.remove();
+                teller.serveCustomerLine();
+                workingTellers.offer(teller);
+                return;
+            }
+            Teller teller = new Teller(customers);
+            executorService.execute(teller);
+            workingTellers.add(teller);
+            return;
+        }
+        if (workingTellers.size() > 1 && customers.size() / workingTellers.size() < 2)
+            reassignOneTeller();
+        if (customers.size() == 0)
+            while (workingTellers.size() > 0)
+                reassignOneTeller();
+
+    }
+
+    private void reassignOneTeller() {
+        Teller teller = workingTellers.poll();
+        teller.doSomethingElse();
+        tellersDoingOtherThings.offer(teller);
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()) {
+                TimeUnit.MILLISECONDS.sleep(adjustmentPeriod);
+                adjustTellerNumber();
+                System.out.print(customers + " { ");
+                for (Teller teller : workingTellers) {
+                    System.out.print(teller.shortString() + " ");
+                }
+                System.out.println("}");
+            }
+        } catch (InterruptedException e) {
+            System.out.println(this + " interrupted");
+        }
+        System.out.println(this + " terminating");
+    }
+
+    @Override
+    public String toString() {
+        return "TellerManager ";
+    }
+}
+
+public class BankTellerSimulation {
+    static final int MAX_LINE_SIZE = 50;
+    static final int ADJUSTMENT_PERIOD = 1000;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        CustomerLine customers = new CustomerLine(MAX_LINE_SIZE);
+        executorService.execute(new CustomerGenerator(customers));
+        executorService.execute(new TellerManager(executorService, customers, ADJUSTMENT_PERIOD));
+        TimeUnit.SECONDS.sleep(10);
+        executorService.shutdownNow();
+    }
+}
